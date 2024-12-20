a615a6c6ab90955b45846373d6e4a5237263a63f
Logan <logmull@gmail.com>
2024-12-20 12:41:01 +0000

    Added basic ability to pull choice data. lots more parsing is needed.

--- README.md
+++ README.md
@@ -5,3 +5,33 @@ https://www.reddit.com/r/humblebundles/comments/11kx7i8/comment/jb9qupl/?utm_sou
 
 Referencing this for vue and express setup
 https://medium.com/@hminnovance/create-a-modern-web-app-with-node-express-and-vue-js-a3e5f224d6d5
+
+
+## TODO
+    Retrieve games from Choice
+    
+    Somehow indicate if the game on choice is available or not - Mostly likely by verifying it is choice and there are 0 choices left and game in unclaimed.
+        Maybe just skip it in this case?
+    Add some theming and styling to make it look half decent
+
+    See if we can dynamically determine the URL for the steam store from the steam app Id
+        https://store.steampowered.com/app/957960 
+        Maybe filter out things tat are not steam based?
+    
+    STEAM API
+        Figure out how to look up the game
+        Video on game stats - wonder if there is any clue to how the API was scraped?
+            https://www.youtube.com/watch?v=qiNv3qv-YbU
+        Look up tags for game, review score
+        
+        Set up DB to store these things so that it does not need run more than once
+        Can use something like this to get data, gives a ton of info, limit of 1 per sec
+        https://steamspy.com/api.php?request=appdetails&appid=730
+    
+    Add ability to upload a file to parse for people other than me to use lol
+
+    Grid filtering
+        By tag
+        Hide claimed items
+        Hide choice
+    
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import fs from 'node:fs';
 import cors from 'cors';
 import express from "express";
+import axios from 'axios';
+import * as cheerio from 'cheerio';
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -34,8 +36,10 @@ app.get("/sample", function(req, res) {
         console.log(uniqueCats)
         // Now build a list of things valid to show in the grid
         // Start with storefront, those are pretty easy.
+        let skip=false;
         const gridData=[];
         for (let item of json){
+          if (skip) continue;
           let data = {};
           if (item.product.category=='storefront'){
           
@@ -43,6 +47,10 @@ app.get("/sample", function(req, res) {
           }
           if (item.product.category=='bundle'){
             processBundleItem(item,gridData);
+          }
+          if (item.product.choice_url){
+            processChoiceItem(item,gridData);
+            skip=true
           }
         }
         return res.send(gridData)
@@ -79,3 +87,33 @@ function processBundleItem(item,gridData){
 app.listen(3000, function(){
     console.log('Listening on port 3000');
 });
+async function processChoiceItem(item,gridData){
+  const scriptbody = fetchScriptContent('https://www.humblebundle.com/membership/'+item.product.choice_url,'webpack-monthly-product-data')
+  console.log(scriptbody)
+}
+/// Get a webpage and parse it to find a script tag. For example
+//  https://www.humblebundle.com/membership/october-2022 id =webpack-monthly-product-data
+
+async function fetchScriptContent(url, scriptId) {
+  try {
+    // Fetch the HTML content
+    const response = await axios.get(url);
+
+    // Load the HTML into cheerio
+    const $ = cheerio.load(response.data);
+
+    // Extract the contents of the script with the specified id
+    const scriptContent = $(`script#${scriptId}`).html();
+
+    if (scriptContent) {
+      console.log(`Contents of <script id="${scriptId}">:\n`, scriptContent);
+      return scriptContent;
+    } else {
+      console.log(`No <script id="${scriptId}"> found in the page.`);
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching the page:', error.message);
+    return null;
+  }
+}
